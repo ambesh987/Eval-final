50ffd64dc23603c4a90080d7ab76e04a9213acb3
ambesh987 <54496820+ambesh987@users.noreply.github.com>
2022-05-07 17:15:03 +0530

    update

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -1,4 +1,9 @@
 const userModel=require("../Models/users");
+const bookModel = require("../Models/book");
+const CommentModel = require("../Models/comment");
+const jwtService = require("../CommonLib/jwtToken")
+const { default: mongoose } = require("mongoose");
+const middlewares = require("../Middlewares/Validators")
 
 async function Register(req,res){
     try{
@@ -38,7 +43,7 @@ async function CreateBook(req,res){
         detail.timestamps=new Date();
         detail.coverImage=path;
         detail.userId=mongoose.Types.Objectid(userId);
-        let res=await BookModel.insertMany([detail]);
+        let res=await bookModel.insertMany([detail]);
         console.log(res);
         res.status(200).json({
            status:"success",
@@ -52,4 +57,43 @@ async function CreateBook(req,res){
         
     }
 }
-module.exports={Register, CreateBook}
+async function Comments(req,res){
+    try {
+      let details = req.body;
+      let userId = req.headers.userId;
+      let bookId = req.headers.bookId;
+      details.userId = mongoose.Types.ObjectId(userId);
+      details.bookId = mongoose.Types.ObjectId(bookId);
+      let result = await CommentModel.insertMany([details]);
+      console.log(result);
+      res.status(200).json({
+        status: "Success",
+        Result: result
+      })
+    } catch (error) {
+      res.status(400).json({
+        Error : error
+      })
+    }
+  }
+  async function Login(req,res,next){
+    let details = req.body;
+    let userDetail = await userModel.findOne({email:details.email});
+    if(userDetail){
+      let token = jwtService.generateToken(details);
+      res.json({
+       status: "Login Successful",
+       token: token
+      })
+    }else{
+      res.status(200).json({
+        status: "Failed",
+        message: "You are not registered with us"
+      });
+    }
+  }
+
+  async function GetAllBooks(req,res,next){
+
+  }
+module.exports={Register, CreateBook, Comments, Login, GetAllBooks}
