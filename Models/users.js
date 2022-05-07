8c133bc0d97f8b7c494fdc5e39e62a95c3c20567
ambesh987 <54496820+ambesh987@users.noreply.github.com>
2022-05-07 17:36:19 +0530

    updated

--- Models/users.js
+++ Models/users.js
@@ -1,8 +1,8 @@
 const mongoose= require('mongoose');
 const user= new mongoose.Schema({
     
-    'firstName':{type:String, required:true},
-    'lastName':{type:String},
+    'firstName':{type:String, required:true, min:3, max:30},
+    'lastName':{type:String, min:3, max:3},
     'age':{type:Number, required:true, min:1, max:150},
     'email':{type:String, required:true, unique:true},
     'profileImages':[{type:Buffer}],
